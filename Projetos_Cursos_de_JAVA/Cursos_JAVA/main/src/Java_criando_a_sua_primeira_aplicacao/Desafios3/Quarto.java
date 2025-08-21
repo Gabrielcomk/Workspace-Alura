d38afab75067604ad968e979ed2191b4f409716d
Gabriel <116319547+Gabrielcomk@users.noreply.github.com>
2025-08-21 11:49:35 -0300

    Delete Projetos_Cursos_de_JAVA/Cursos_JAVA directory

--- Projetos_Cursos_de_JAVA/Cursos_JAVA/main/src/Java_criando_a_sua_primeira_aplicacao/Desafios3/Quarto.java
+++
@@ -1,7 +0,0 @@
-package Java_criando_a_sua_primeira_aplicacao.Desafios3;
-
-public class Quarto {
-    public static void main(String[] args){
-
-    }
-}
--- Projetos_Cursos_de_JAVA/Cursos_JAVA/main/src/Java_trabalhando_com_listas_e_colecoes_de_dados/br/com/alura/screenmatch/calculos/Classificavel.java
+++
@@ -1,5 +0,0 @@
-package Java_trabalhando_com_listas_e_colecoes_de_dados.br.com.alura.screenmatch.calculos;
-
-public interface Classificavel {
-    int getClassificacao();
-}
